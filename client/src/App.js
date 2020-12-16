0195861208b8fa31709e14e9614f3b5d42e56394
Jordan Kirby <jrock255@hotmail.com>
2020-12-15 21:40:46 -0700

    made a simple narvbar with a signup and login page plus simple router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+import "./App.css";
 
 function App() {
   return (
-   <div>Project 3</div>
+    <Router>
+      <div className="App">
+        <NavBar />
+
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/SignupPage" component={SignupPage} />
+          <Route exact path="/LoginPage" component={LoginPage} />
+        </Switch>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div>
+      <footer className="footer">
+        <span>
+          <h5>Made by Blue Team</h5>
+        </span>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
