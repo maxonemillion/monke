b2938d35f1980c3812db495f2a86de5d5787ab54
Jordan Kirby <jrock255@hotmail.com>
2020-12-17 21:05:45 -0700

    made a form for signup and login page

--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 function LoginPage() {
     return (
-        <div>
-            <h1>Login Page</h1>
-        </div>
-    )
+      <div>
+        <Container>
+          <Form>
+            <Form.Group controlId="formBasicLogin">
+              <Form.Label>User Name</Form.Label>
+              <Form.Control id="login-email" type="login" placeholder="Name" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>User Password</Form.Label>
+              <Form.Control id="login-password" type="password" placeholder="Password" />
+            </Form.Group>
+            <Button className="mb-5" disabled id="login" variant="primary" type="login">
+              Login
+            </Button>
+          </Form>
+        </Container>
+      </div>
+    );
 }
 
 export default LoginPage;
--- client/src/pages/SignupPage/SignupPage.jsx
+++ client/src/pages/SignupPage/SignupPage.jsx
@@ -1,39 +1,39 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-
 
 function SignupPage() {
   return (
     <div>
       <Container>
-        <Row>
-          <Col>
-            <Form>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
+        <Form>
+          <Form.Group controlId="formBasicSignup">
+            <Form.Label>Create User Name</Form.Label>
+            <Form.Control id="user-email" type="email" placeholder="Name" />
+          </Form.Group>
 
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Form>
-          </Col>
-        </Row>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Create Password</Form.Label>
+            <Form.Control
+              id="user-password"
+              type="password"
+              placeholder="Password"
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Button
+            className="mb-5"
+            disabled
+            id="create-user-account"
+            variant="primary"
+            type="signup"
+          >
+            Signup
+          </Button>
+        </Form>
       </Container>
     </div>
   );
