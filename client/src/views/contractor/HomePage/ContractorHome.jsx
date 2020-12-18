8a49f4a951bf4f04e0a0fb19680789aa5601ec0f
maxonemillion <max.daniel@utah.edu>
2020-12-18 16:01:45 -0700

    views

---
+++ client/src/views/contractor/HomePage/ContractorHome.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function ContractorHome() {
+  return(
+    <h2>Contractor Home</h2>
+  );
+}
--- client/src/views/worker/worker.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function WorkerHome() {
-  return(
-    <h2>WORKER</h2>
-  );
-}
