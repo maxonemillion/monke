48b5f839ecd60a761f5e4fadf2c473979105ab33
maxonemillion <max.daniel@utah.edu>
2020-12-18 23:08:42 -0700

    restarting auth

--- server/routes/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const auth = require('./auth-routes.js');
-
-// /api/todo
-router.use('/api/auth', auth);
-
-module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
-const passport = require("./config/passport");
-const session = require("express-session");
 const express = require('express');
 const routes = require('./routes');
 const path = require('path');
@@ -8,21 +6,14 @@ require('./config/db')();
 
 const PORT = process.env.PORT || 3000;
 
-// parsing middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-app.use(session({ secret: "project", resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
-
-// if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/"));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/public/index.html"));
-  });
-// }
+app.use(express.static("client/"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log('app running on PORT: ' + PORT);
