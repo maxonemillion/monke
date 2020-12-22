06a4eba754700a84ccc63a75d6b636b46de9d10c
maxonemillion <max.daniel@utah.edu>
2020-12-21 17:30:33 -0700

    backend setup

--- server/server.js
+++ server/server.js
@@ -1,18 +1,23 @@
 const express = require('express');
-const routes = require('./routes');
+// const routes = require('./routes');
+const controller = require("./controller");
 const path = require('path');
 const app = express();
-require('./config/db')();
+// require('./config/db')();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+// app.use(routes);
+app.post('/login', (req, res) => {
+  controller.login(req.body);
+  res.send("ok")
+});
 
-app.use(express.static("client/"));
+app.use(express.static("../client/build"));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 app.listen(PORT, () => {
