bfc9454c572b1e2ae03e2b4e5cdd2b9fad4e9f61
Jordan Kirby <jrock255@hotmail.com>
2020-12-23 03:52:55 -0700

    added some styles to the homepage

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,26 +1,70 @@
 import React from "react";
+import { Jumbotron, Container, Card } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import Binary from "../../components/video/BinaryRainFHDLoop.webm";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+// import Binary from "../../components/video/BinaryRainFHDLoop.webm";
 import "./HomePage.css";
 
 const HomePage = () => {
   return (
-    <div id="homePage">
+    <div className="home-page" id="homePage">
       <div id="viewContent">
-      <video autoPlay loop muted>
-        <source src={Binary} type="video/webm" />
-      </video>
-      <h1>Welcome, Coders!</h1>
-      <p>
-        <Button id="getStarted" variant="danger">
-          Get Started!
-        </Button>
-      </p>
-      <p>
-        <Button id="loginHome" variant="secondary">
-          Login Here!
-        </Button>
-      </p>
+        {/* <video autoPlay loop muted>
+          <source src={Binary} type="video/webm" />
+        </video> */}
+        <Jumbotron>
+          <h1 className="display-1">Welcome, Coders!</h1>
+          <Row>
+            <Col>
+              <Button size="lg" id="getStarted" variant="danger">
+                Get Started!
+              </Button>
+
+              <Button size="lg" id="loginHome" variant="secondary">
+                Login Here!
+              </Button>
+            </Col>
+          </Row>
+        </Jumbotron>
+
+        <Container>
+          <Row>
+            <Col>
+              <Card className="listing">
+                <Card.Body>
+                  <Card.Title>
+                    <h2>Card Title</h2>
+                  </Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    Card Subtitle
+                  </Card.Subtitle>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col>
+              <Card className="listing">
+                <Card.Body>
+                  <Card.Title>
+                    <h2>Card Title</h2>
+                  </Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    Card Subtitle
+                  </Card.Subtitle>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
       </div>
     </div>
   );
