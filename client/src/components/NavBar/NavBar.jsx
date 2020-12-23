2a8ccd7bb2189a953b964821748bf05c4630fead
Jordan Kirby <jrock255@hotmail.com>
2020-12-23 00:48:41 -0700

    made the navbar more responsive added some styles

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,21 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Navbar, Nav, Button, Container } from "react-bootstrap";
 import "./NavBar.css";
 
-const Navbar = () => {
+const Navbars = () => {
   return (
-    <nav className="Navbar">
-      <div className="Navbar-brand">
-        <Link to="/">Project 3</Link>
-      </div>
-
-      <ul className="Navbar-links">
-        <li className="Navbar-link">
-          <Link to="/LoginPage">Login</Link>
-        </li>
-      </ul>
-    </nav>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand className="brand"  href="/">
+          Project3
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Button variant="primary" size="lg">
+              <Link style={{ color: "#ffffff" }} to="/LoginPage">
+                Login
+              </Link>
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default Navbars;
+
+//  <nav className="Navbar">
+//       <div className="Navbar-brand">
+//         <Link to="/">Project 3</Link>
+//       </div>
+
+//       <ul className="Navbar-links">
+//         <li className="Navbar-link">
+//           <Link to="/LoginPage">Login</Link>
+//         </li>
+//       </ul>
+//     </nav>
